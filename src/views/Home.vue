ba0b57be9e41a43535b0c14ca7732ab18e7b7492
ffrastio <48426886+ffrastio@users.noreply.github.com>
2021-05-31 08:47:15 +0700

    export data grafik to pdf

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,60 +38,64 @@
     <!-- Start Data Statistik -->
     <div class="flex justify-between items-center">
       <div class="text-xl text-left font-bold py-4 px-4">Data Statistik</div>
-      <div class="px-4"
-        
-      >
-        <a href="#" id="downloadPdf" class="bg-primary hover:bg-primary-200 text-white hover:text-white rounded shadow hover:shadow-lg py-2 px-2 border"><font-awesome-icon icon="file-pdf" class="mr-2" />Download Report</a>
+      <div class="px-4">
+        <button
+          href="#"
+          id="downloadPdf"
+          class="bg-primary hover:bg-primary-200 text-white hover:text-white rounded shadow hover:shadow-lg py-2 px-2 border"
+          @click="exportToPDF(0)"
+        >
+          <font-awesome-icon icon="file-pdf" class="mr-2" />Download Report
+        </button>
       </div>
     </div>
     <div
       class="bg-gray-200 px-4 py-4 grid grid-cols-1 gap-2 md:grid-cols-2 mb-4 animate-popup"
     >
-    <div class="bg-white text-left">
-      <select class="dropdown border rounded px-1 py-1 mt-2 ml-6" id="dd">
-    <option value="" selected="selected">Pilih Tahun</option>
-    <option value="dps1">2017</option>
-    <option value="dps2">2018</option>
-    <option value="dps3">2019</option>
-    <option value="dps4">2020</option>
-    <option value="dps5">2021</option>
-</select>
-      <LineChart1 class="bg-white rounded" />
-    </div>
-    <div class="bg-white text-left">
-      <select class="dropdown border rounded px-1 py-1 mt-2 ml-6" id="dd">
-    <option value="" selected="selected">Pilih Tahun</option>
-    <option value="dps1">2017</option>
-    <option value="dps2">2018</option>
-    <option value="dps3">2019</option>
-    <option value="dps4">2020</option>
-    <option value="dps5">2021</option>
-</select>
-      <LineChart2 class="bg-white rounded" />
-    </div>
-    <div class="bg-white text-left">
-      <select class="dropdown border rounded px-1 py-1 mt-2 ml-6" id="dd">
-    <option value="" selected="selected">Pilih Tahun</option>
-    <option value="dps1">2017</option>
-    <option value="dps2">2018</option>
-    <option value="dps3">2019</option>
-    <option value="dps4">2020</option>
-    <option value="dps5">2021</option>
-</select>
-      <BarChart1 class="bg-white rounded" />
-    </div>
-    <div class="bg-white text-left">
-      <select class="dropdown border rounded px-1 py-1 mt-2 ml-6" id="dd">
-    <option value="" selected="selected">Pilih Tahun</option>
-    <option value="dps1">2017</option>
-    <option value="dps2">2018</option>
-    <option value="dps3">2019</option>
-    <option value="dps4">2020</option>
-    <option value="dps5">2021</option>
-</select>
-      <BarChart2 class="bg-white rounded" />
-    </div>
-
+      <div class="bg-white text-left">
+        <select class="dropdown border rounded px-1 py-1 mt-2 ml-6" id="dd">
+          <option value="" selected="selected">Pilih Tahun</option>
+          <option value="dps1">2017</option>
+          <option value="dps2">2018</option>
+          <option value="dps3">2019</option>
+          <option value="dps4">2020</option>
+          <option value="dps5">2021</option>
+        </select>
+        <LineChart1 class="bg-white rounded" id="chart1" />
+      </div>
+      <div class="bg-white text-left">
+        <select class="dropdown border rounded px-1 py-1 mt-2 ml-6" id="dd">
+          <option value="" selected="selected">Pilih Tahun</option>
+          <option value="dps1">2017</option>
+          <option value="dps2">2018</option>
+          <option value="dps3">2019</option>
+          <option value="dps4">2020</option>
+          <option value="dps5">2021</option>
+        </select>
+        <LineChart2 class="bg-white rounded" id="chart2" />
+      </div>
+      <div class="bg-white text-left">
+        <select class="dropdown border rounded px-1 py-1 mt-2 ml-6" id="dd">
+          <option value="" selected="selected">Pilih Tahun</option>
+          <option value="dps1">2017</option>
+          <option value="dps2">2018</option>
+          <option value="dps3">2019</option>
+          <option value="dps4">2020</option>
+          <option value="dps5">2021</option>
+        </select>
+        <BarChart1 class="bg-white rounded" id="chart3" />
+      </div>
+      <div class="bg-white text-left">
+        <select class="dropdown border rounded px-1 py-1 mt-2 ml-6" id="dd">
+          <option value="" selected="selected">Pilih Tahun</option>
+          <option value="dps1">2017</option>
+          <option value="dps2">2018</option>
+          <option value="dps3">2019</option>
+          <option value="dps4">2020</option>
+          <option value="dps5">2021</option>
+        </select>
+        <BarChart2 class="bg-white rounded" id="chart4" />
+      </div>
     </div>
     <!-- Ends Data Statistik -->
     <Footer />
@@ -106,7 +110,8 @@ import LineChart1 from "@/components/chart/penelitianLineChart.vue";
 import LineChart2 from "@/components/chart/pengabdianLineChart.vue";
 import BarChart1 from "@/components/chart/penelitianBarChart.vue";
 import BarChart2 from "@/components/chart/pengabdianBarChart.vue";
-
+import Exporter from "vue-chartjs-exporter";
+import { jsPDF } from "jspdf"
 export default {
   name: "Home",
   components: {
@@ -116,6 +121,22 @@ export default {
     LineChart2,
     BarChart1,
     BarChart2,
+  },
+  methods: {
+    exportToPDF() {
+      // this.download_status = "Processing...";
+      let chart1 = document.getElementById("chart1");
+      let chart2 = document.getElementById("chart2");
+      let chart3 = document.getElementById("chart3");
+      let chart4 = document.getElementById("chart4");
+      var doc = new jsPDF("p", "pt");
+      doc.text("Laporan Grafik UP2M Politeknik Negeri Jakarta", 240, 40);
+      const exp = new Exporter([chart1, chart2, chart3, chart4]);
+      exp.export_pdf().then((pdf) => {
+        pdf.save("Laporan Grafik.pdf");
+        // this.download_status = "Download Charts";
+      });
+    },
   },
 };
 </script>
